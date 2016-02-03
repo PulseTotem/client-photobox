d9e96695c49cb6a1ce8ab0f42b8b499f6ee79583
Simon Urli <simon.urli@gmail.com>
2016-02-03 16:25:43 +0100

    DEMO-1 Generate code skeleton

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientPhotoboxApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the clientPhotoboxApp
+ */
+angular.module('clientPhotoboxApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientPhotoboxApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the clientPhotoboxApp
+ */
+angular.module('clientPhotoboxApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
