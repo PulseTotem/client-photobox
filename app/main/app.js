0e6933f8c945ecd6509eddffb8aecbdd6945d99d
Simon Urli <simon.urli@gmail.com>
2016-02-10 12:47:05 +0100

    DEMO-1 Setup the photobox client

--- app/main/app.js
+++ app/main/app.js
@@ -18,10 +18,18 @@ angular
     'ngTouch',
     'ngMaterial',
     'pascalprecht.translate',
+    'btford.socket-io',
     'PulseTotemCommon',
     'PulseTotemControl'
     ])
-    .config(['$locationProvider', function($locationProvider) {
-        $locationProvider.html5Mode(true).hashPrefix('!');
-    }])
+  .config(['$locationProvider', function($locationProvider) {
+    $locationProvider.html5Mode(true).hashPrefix('!');
+  }])
+  .config(['$mdThemingProvider', function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+      .primaryPalette('blue-grey', {
+        'default' : '600'
+      })
+      .accentPalette('blue');
+  }])
 ;
