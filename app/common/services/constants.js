92cd8fd06ef4b3c2309d0941ca86430be333016c
Simon Urli <simon.urli@gmail.com>
2016-02-13 13:24:59 +0100

    DEMO-1 Implement translation. Ready to deploy.

--- app/common/services/constants.js
+++ app/common/services/constants.js
@@ -8,16 +8,16 @@
  */
 angular.module('PulseTotemCommon')
     .constant('CONSTANTS', {
-        backendUrl: 'http://localhost:4000/',
-        //backendUrl: 'http://backend.pulsetotem.fr/',
+        //backendUrl: 'http://localhost:4000/',
+        backendUrl: 'http://backend.pulsetotem.fr/',
         //backendUrl: 'http://backend-test.pulsetotem.fr/',
         loginBackendPath: 'login',
         loginFromTokenBackendPath: 'loginFromToken',
         homeRoute: '/',
         afterLoginRoute: '/dashboard',
 
-        photoboxServiceUrl: 'http://localhost:6012/',
-        //photoboxServiceUrl: 'http://service-photobox.pulsetotem.fr/',
+        //photoboxServiceUrl: 'http://localhost:6012/',
+        photoboxServiceUrl: 'http://service-photobox.pulsetotem.fr/',
         //photoboxServiceUrl: 'http://service-photobox-test.pulsetotem.fr/',
         photoboxClientPath: 'PhotoboxClient'
     });
